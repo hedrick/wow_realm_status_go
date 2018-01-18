181912cdaa4f4a32bec0813a549482b9016386fc
Scott Hedrick <me@scottshedrick.com>
2018-01-18 16:08:19 -0600

    initial code to do facile http request for servers list and marshal the data into JSON

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+type realms struct {
+	Servers []server `json:"realms"`
+}
+
+type server struct {
+	Type            string `json:"type"`
+	Population      string `json:"population"`
+	Queue           bool   `json:"queue"`
+	Status          bool   `json:"status"`
+	Name            string `json:"name"`
+	Slug            string `json:"-"`
+	BattleGroup     string `json:"-"`
+	Local           string `json:"-"`
+	Timezone        string `json:"-"`
+	ConnectedRealms string `json:"-"`
+}
+
+const rURL = "https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=m8m9as776592afjxmwx45vy8yabgpngb"
+
+var realm = flag.String("realm", "medivh", "the individual realm's status to check")
+
+func main() {
+	flag.Parse()
+	r := *realm
+	getServerStatus(r)
+}
+
+func getAllServers() []server {
+	realms := realms{}
+	cl := http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, rURL, nil)
+	if err != nil {
+		fmt.Println("Request Error:", err)
+		os.Exit(1)
+	}
+
+	req.Header.Set("User-Agent", "wow_realm_status_go")
+
+	res, err := cl.Do(req)
+	if err != nil {
+		fmt.Println("Get Error:", err)
+		os.Exit(1)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Read Error:", err)
+		os.Exit(1)
+	}
+
+	jsonErr := json.Unmarshal(body, &realms)
+	if jsonErr != nil {
+		fmt.Println("JSON Error:", jsonErr)
+	}
+	s := realms.Servers
+	// for _, s := range realms.Servers {
+	// 	fmt.Println(s.Name)
+	// }
+	return s
+}
+
+func getServerStatus(name string) {
+	servers := getAllServers()
+	for _, server := range servers {
+		status := boolToStatusString(server.Status)
+		if server.Name == name {
+			fmt.Printf("%s is %s!", name, status)
+		}
+	}
+}
+
+func boolToStatusString(status bool) string {
+	if status == true {
+		return "up"
+	}
+	return "down"
+}
